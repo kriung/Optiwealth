7d50cc3b3478cd1a957bdc751f7452f245163061
kriung <mtembeikevin@gmail.com>
2024-05-07 13:31:53 +0300

    Modified streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import yfinance as yf
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-
 import plotly.express as px
 import streamlit as st
 from PIL import Image
